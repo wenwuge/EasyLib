b5a87f869d240b69b607c43954cc9eb89dd06b71
libin3-s <libin3-s@video02v.ops.corp.qihoo.net>
2015-09-18 11:03:45 +0800

    support noncopyable class

--- noncopyable.h
+++ noncopyable.h
@@ -1,22 +1,19 @@
 #ifndef OSLIB_NONCOPYABLE_H_
 #define OSLIB_NONCOPYABLE_H_
 
-namespace osl
+class Noncopyable 
 {
-	class Noncopyable 
-	{
-    protected:
-        Noncopyable () {};
-        ~Noncopyable () {};
-	private:
-		Noncopyable ( const Noncopyable& );
-		Noncopyable &operator=( const Noncopyable& );
-	};
-    typedef Noncopyable noncopyable;
+protected:
+    Noncopyable () {};
+    ~Noncopyable () {};
+private:
+    Noncopyable ( const Noncopyable& );
+    Noncopyable &operator=( const Noncopyable& );
+};
+typedef Noncopyable noncopyable;
 
-    class copyable
-    {
-    };
-}
+class copyable
+{
+};
 
-
+#endif 
